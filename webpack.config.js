b982c2f954b3a1eb587fdb19b062d7326287bd44
abdelali-code <57986145+abdelali-code@users.noreply.github.com>
2020-03-11 22:31:38 +0100

    bundle css and images

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
 
 
-module.exports = {
+  module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    }
-}
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+   module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader',
+         ],
+       },
+       {
+         test: /\.(png|svg|jpg|gif)/,
+         use: [
+           'file-loader',
+         ],
+       }
+     ],
+   },
+  };
